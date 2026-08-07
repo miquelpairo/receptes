49af7219198c15599bf89dac3531d7684e1c8d63
Miquel Pairó <pairo.miquel@gmail.com>
2026-08-07 20:28:07 +0200

    Afegir fitxer de dades de receptes

---
+++ receptes_dades.js
@@ -0,0 +1,25 @@
+const receptesData = [
+    {
+        name: "Arròs a la cubana",
+        tags: ["Arrossos", "Tradicional"],
+        ingredients: [
+            "Arròs",
+            "Tomàquet fregit",
+            "OUS",
+            "Plàtan"
+        ],
+        instructions: "1. Coure l'arròs amb aigua i sal.\n2. Fregir els plàtans i els ous.\n3. Servir calent amb salsa de tomàquet."
+    },
+    {
+        name: "Crema catalana",
+        tags: ["Postres", "Dolç"],
+        ingredients: [
+            "Llet",
+            "Llets d'ou",
+            "Sucre",
+            "Farina de blat de moro",
+            "Canella i llimona"
+        ],
+        instructions: "1. Infusionar la llet amb la pell de llimona i la canella.\n2. Barrejar els rovells amb sucre i farina.\n3. Coure a foc lent fins que espesseixi i cremar sucre per damunt."
+    }
+];
